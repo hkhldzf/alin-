580e760417ebad5255a7adaa6e75adb8ffc6128c
hkhldzf <923331604@qq.com>
2021-05-04 22:03:18 +0800

    first commit

---
+++ SuperClient.java
@@ -0,0 +1,77 @@
+package Client;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class SuperClient {
+	
+	private static String _name = "";
+	
+	public static void main(String[] args) {
+		
+		try {
+			Socket s = new Socket("localhost",6666);
+			
+			OutputStream out = s.getOutputStream();
+			DataOutputStream dos = new DataOutputStream(out);
+			
+			GetMessageThread gmt = new GetMessageThread(s);
+			gmt.start();
+			
+			Scanner sc = new Scanner(System.in);
+			
+			while(true)
+			{
+				System.out.println("请输入消息类别,101登录，102发消息");
+				int num = sc.nextInt();
+				if(num == 101)
+				{
+					//登录
+					System.out.println("请输入用户名");
+					sc = new Scanner(System.in);
+					String name = sc.nextLine();
+					_name = name;
+					Message m = new Message();
+					m.setCode(101);
+					m.setName(name);
+					//obj -> json str
+					String msg = m.getMessage();
+					dos.writeUTF(msg);
+					dos.flush();
+				}
+				else if(num == 102)
+				{
+					//发消息
+					System.out.println("请输入to谁");
+					sc = new Scanner(System.in);
+					String to = sc.nextLine();
+					
+					System.out.println("请输入消息内容");
+					sc = new Scanner(System.in);
+					String msg = sc.nextLine();
+					
+					Message m = new Message();
+					m.setCode(102);
+					m.setName(_name);
+					m.setTo(to);
+					m.setMessage(msg);
+					
+					//obj -> json str	
+					String str = m.getMessage();
+					System.out.println(str);
+					dos.writeUTF(str);
+					dos.flush();
+				}
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
